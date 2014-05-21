27d8adabe070099ff8355aefaa3fc717a66b16ab
Oren <ogolan@yp.com>
2014-05-21 11:28:33 -0700

    init

---
+++ bundle.js
@@ -0,0 +1,20 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var Widget = require('./widget.js');
+var w = new Widget();
+
+w.appendTo('#container');
+
+},{"./widget.js":2}],2:[function(require,module,exports){
+module.exports = Widget;
+
+function Widget (opts) {
+    if (!(this instanceof Widget)) return new Widget(opts);
+    this.element = document.createElement('div');
+}
+
+Widget.prototype.appendTo = function (target) {
+    if (typeof target === 'string') target = document.querySelector(target);
+    target.appendChild(this.element);
+};
+
+},{}]},{},[1])
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Reusable Components using Browserify
+
+* [Build](#build)
+* [Run](#run)
+
+## Build
+    npm run build-js
+
+## Run
+    open index.html
