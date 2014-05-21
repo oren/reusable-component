af1dc2c105f219c8514e8a2af32dd10166f937ed
oren <orengolan@gmail.com>
2014-05-21 13:28:27 -0700

    Update app.js

--- module/app.js
+++ module/app.js
@@ -1,10 +1,12 @@
 var Widget = require('app-widget');
 var w = new Widget();
 
+// app-widget can emit events
 w.on('append', function (target) {
     console.log('appended to: ' + target.outerHTML);
 });
 
+// calling some functions on app-widget
 w.appendTo('#container');
 w.setName('widget');
 w.setMessage('I live in node_modules/app-widget folder');
